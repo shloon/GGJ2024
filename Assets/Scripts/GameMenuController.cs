6d524baf5139505a16accbc89477798e5df559e1
Shloon <shlomo.shmouely@gmail.com>
2024-01-26 10:47:04 +0200

    Refactor game over menu

--- Assets/Scripts/GameMenuController.cs
+++ Assets/Scripts/GameMenuController.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class GameOverMenuController : MonoBehaviour
+public class GameMenuController : MonoBehaviour
 {
     // Start is called before the first frame update
     public void RetryGame()
