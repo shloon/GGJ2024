b3d02a3073ff0c3bf1e8b33d2f602a7789ad705e
LehMipo412 <barbetzalel412@gmail.com>
2024-01-27 02:46:56 +0200

    made cool sound thingy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,6 +31,13 @@ public class PlayerController : MonoBehaviour
     public float gunStaminaConsumption;
     public bool isShooting;
 
+    [Header("Sounds")]
+    public AudioSource playerAudioScource;
+    public AudioClip gettingHurtSound1;
+    public AudioClip gettingHurtSound2;
+    public AudioClip gettingHurtSound3;
+    //public AudioClip walkingSound;
+    //public AudioClip shootingSound;
     void Start()
     {
         staminaBar.value = 1;
@@ -88,6 +95,7 @@ public class PlayerController : MonoBehaviour
             theGun.SetActive(false);
             theSpray.SetActive(false);
             isShooting = false;
+            //playerAudioScource.Stop();
         }
 
         // stamina increasing automagically
@@ -136,8 +144,10 @@ public class PlayerController : MonoBehaviour
         else
         {
             playerMovement = movement * speed;
+            //playerAudioScource.PlayOneShot(walkingSound);
         }
         playerRB.velocity = playerMovement;
+
     }
 
     public void KillPlayer()
@@ -147,6 +157,13 @@ public class PlayerController : MonoBehaviour
 
     public void TakeDamage()
     {
+        int rand = Random.Range(1, 4);
+        if (rand == 1)
+            playerAudioScource.PlayOneShot(gettingHurtSound1);
+        if (rand == 2)
+            playerAudioScource.PlayOneShot(gettingHurtSound2);
+        if (rand == 3)
+            playerAudioScource.PlayOneShot(gettingHurtSound3);
         hpBar.value -= 0.1f;
     }
 
